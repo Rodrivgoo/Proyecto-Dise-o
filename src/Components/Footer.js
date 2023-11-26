793c70e9454fae2c8e2218c9712d6b17388489ff
Rodrigo <rodrigo.gomezr21@gmail.com>
2023-11-25 23:42:40 -0300

    Malcom Lats version

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <div className="social-icons">
                 <a href="https://www.instagram.com/Rodrivgoo" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
                 <a href="https://github.com/Rodrivgoo/Proyecto-Cartelera" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                <a href="https://www.instagram.com/br0wed_" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             </div>
         </footer>
     );
