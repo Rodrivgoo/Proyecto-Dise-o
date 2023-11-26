07f498bf0993486b257265ae08712fed87aeaedb
Rodrigo <rodrigo.gomezr21@gmail.com>
2023-11-26 12:45:51 -0300

    Footer Update Link Github

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className="footer">
             <div className="social-icons">
                 <a href="https://www.instagram.com/Rodrivgoo" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-                <a href="https://github.com/Rodrivgoo/Proyecto-Cartelera" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                <a href="https://github.com/Rodrivgoo/Proyecto-Cartelera/tree/master" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
                 <a href="https://www.instagram.com/br0wed_" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             </div>
         </footer>
